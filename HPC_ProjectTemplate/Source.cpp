175b3c20fd94ab564ae100ae4063719575596891
Kirlos Melad <kirlosmelad2000@gmail.com>
2021-12-17 15:53:11 +0200

    reShape the project to be more fit for Background Subtraction

--- HPC_ProjectTemplate/Source.cpp
+++ HPC_ProjectTemplate/Source.cpp
@@ -4,6 +4,7 @@
 #include<string.h>
 #include<msclr\marshal_cppstd.h>
 #include <ctime>// include this header 
+#include <mpi.h>
 #pragma once
 
 #using <mscorlib.dll>
@@ -14,9 +15,10 @@ using namespace std;
 using namespace msclr::interop;
 
 struct Image {
-	int* const _img, const _height, const _width;
+	int* const _img;
+	unsigned int const _height, const _width;
 
-	Image(int* img, int h, int w) : _img(img), _height(h), _width(w){}
+	Image(int* img, unsigned h, unsigned w) : _img(img), _height(h), _width(w){}
 	~Image() {
 		free((int*)_img); // https://stackoverflow.com/questions/2819535/unable-to-free-const-pointers-in-c
 	}
@@ -67,7 +69,7 @@ int* inputImage(int* w, int* h, System::String^ imagePath) //put the size of ima
 }
 
 
-void createImage(int* image, int width, int height, int index)
+void createImage(int* image, int width, int height, string rsltName, int index)
 {
 	System::Drawing::Bitmap MyNewImage(width, height);
 
@@ -89,38 +91,107 @@ void createImage(int* image, int width, int height, int index)
 			MyNewImage.SetPixel(j, i, c);
 		}
 	}
-	MyNewImage.Save("..//Data//Output//outputRes" + index + ".png");
-	cout << "result Image Saved " << index << endl;
+	MyNewImage.Save("..//Data//Output//" + marshal_as<System::String^>(rsltName) + index + ".png");
+	cout << "result Image Saved " << rsltName << index << endl;
+}
+
+vector<Image> readInput() {
+	System::String^ imagePath;
+	string const path = "..//Data//Input//";
+	string imgName;
+
+	vector<Image> imgVec;
+	int *ImageArr, ImageWidth, ImageHeight, const imgNumber = 495, const totalDigits = 6;
+
+	for (int i = 1; i <= imgNumber; i++) {
+		// Get Img Name
+		imgName = "in";
+		string num = to_string(i);
+
+		int const internalLoop = totalDigits - num.size();
+		for (int j = 0; j < internalLoop; j++) {
+			imgName += '0';
+		}
+		imgName += num;
+
+		// Get Img
+		imagePath = marshal_as<System::String^>(path + imgName);
+		ImageArr = inputImage(&ImageWidth, &ImageHeight, imagePath);
+
+		// Append Img to the vector
+		imgVec.emplace_back(ImageArr, ImageHeight, ImageWidth);
+	}
+
+	return imgVec;
+}
+
+void saveOutput(vector<Image> imgVec, string rsltName) {
+	for (int i = 0; i < imgVec.size(); i++) {
+		createImage(imgVec[i]._img, imgVec[i]._width, imgVec[i]._height, rsltName,  i + 1);
+	}
+}
+
+vector<Image> muskExtraction(vector<Image> bgVec, int const TH) {
+	vector<Image> mskVec;
+
+	// Initialize the MPI env
+	MPI_Init(NULL, NULL);
+
+	// Get processes number
+	int wSize;
+	MPI_Comm_size(MPI_COMM_WORLD, &wSize);
+
+	// Get rank of this process
+	int wRank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &wRank);
+
+	// Finalize MPI env
+	MPI_Finalize();
+
+	return mskVec;
+}
+
+vector<Image> backgroundExtraction(vector<Image> imgVec) {
+	vector<Image> bgVec;
+
+	// Initialize the MPI env
+	MPI_Init(NULL, NULL);
+
+	// Get processes number
+	int wSize;
+	MPI_Comm_size(MPI_COMM_WORLD, &wSize);
+
+	// Get rank of this process
+	int wRank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &wRank);
+
+	// Finalize MPI env
+	MPI_Finalize();
+
+	return bgVec;
 }
 
 
-int main()
+int main(int argc, char **argv)
 {
-	int ImageWidth = 4, ImageHeight = 4;
+	// Get Img Vector
+	vector<Image> imgVec = readInput();
 
 	int start_s, stop_s, TotalTime = 0;
-
-	System::String^ imagePath;
-	std::string img;
-	img = "..//Data//Input//test.png";
-
-	imagePath = marshal_as<System::String^>(img);
-	//int* imageData = inputImage(&ImageWidth, &ImageHeight, imagePath);
-	Image imageData(inputImage(&ImageWidth, &ImageHeight, imagePath), ImageHeight, ImageWidth);
-	
 	start_s = clock();
-	/*
 
-	*/
+	vector<Image> bgVec = backgroundExtraction(imgVec);
+	vector<Image> mskVec = muskExtraction(bgVec, 20);
+	
 	stop_s = clock();
+	// Get Parrallelism time
 	TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
-	createImage(imageData._img, ImageWidth, ImageHeight, 1);
 	cout << "time: " << TotalTime << endl;
 
-	//free(imageData._img);
+	// Save background and musk Vectors
+	saveOutput(bgVec, "Background");
+	saveOutput(mskVec, "Musk");
+
 	return 0;
 
 }
-
-
-
